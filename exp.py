269ee7a448c84d5ba4123b701d33d2ce58ed2194
Elirant <elias@schroeter-net.eu>
2026-06-01 23:33:10 +0200

    Create exp.py

---
+++ exp.py
@@ -0,0 +1,7 @@
+hallo = ["Hallo","Hello","Hi"]
+answer = input("Say youre Hello variation")
+for right in hallo:
+  if answer != right:
+    print("Youre not normal")
+  else:
+    print("Youre normal")
