20a5b3bdb7d98b8d7c486a4d19e37520ae8a3860
Elirant <elias@schroeter-net.eu>
2026-06-01 21:09:55 +0000

    Welcomer.sh

---
+++ welcomer.sh
@@ -0,0 +1,23 @@
+echo "Hallo und willkommen in deinem Linux System"
+echo -ne "="
+sleep 0.5
+clear
+echo -ne "=="
+sleep 0.5
+clear
+echo -ne "==="
+sleep 0.5
+clear
+echo -ne "===[|]"
+sleep 0.5
+clear
+echo -ne "===[/]="
+sleep 0.5
+clear
+echo -ne "===[-]=="
+sleep 0.5
+clear
+echo -ne "===[\]==="
+sleep 0.5
+clear
+echo -ne "===[|]==="
